29b7dbac12dbc5eb356976380527612d93073a86
afeiship <1290657123@qq.com>
2019-03-17 12:47:56 +0800

    add: curd+routes

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,17 +1,38 @@
 class OptionsController < ApplicationController
+    before_action :set_item, only: [:show, :destroy, :update]
+
     def index
         @items = Option.all
         json_response(@items)
-        # render json: @items, status: :ok
     end
     
-    def create
-        @item = Option.new(allow_params)
-        @item.save
+    def show
+        json_response(@item)
     end
 
-    private 
+    def create
+        @item = Option.new(allow_params)
+        json_response(@item.save)
+    end
+
+    def update
+        @item.update allow_params if @item
+        json_response(@item)
+    end
+
+    def destroy
+        @item.destroy if @item
+        json_response(@item)
+    end
+
+
+    private
+
     def allow_params
         params.require(:option).permit!
+    end
+
+    def set_item
+        @item = Option.find(params[:id]) rescue nil
     end
 end
--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,2 +1,38 @@
 class ThemesController < ApplicationController
+    before_action :set_item, only: [:show, :destroy, :update]
+
+    def index
+        @items = Theme.all
+        json_response(@items)
+    end
+    
+    def show
+        json_response(@item)
+    end
+
+    def create
+        @item = Theme.new(allow_params)
+        json_response(@item.save)
+    end
+
+    def update
+        @item.update allow_params if @item
+        json_response(@item)
+    end
+
+    def destroy
+        @item.destroy if @item
+        json_response(@item)
+    end
+
+
+    private
+
+    def allow_params
+        params.require(:theme).permit!
+    end
+
+    def set_item
+        @item = Theme.find(params[:id]) rescue nil
+    end
 end
